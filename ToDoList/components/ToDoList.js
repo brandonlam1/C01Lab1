38d4e603b30bc9841677ee210eb2ed005a0176ad
Brandon <brandon.lam@mail.utoronto.ca>
2024-01-21 23:16:22 -0500

    ToDoList now shows initial todo list items on expo. Also able to remove todo list items.

--- ToDoList/components/ToDoList.js
+++ ToDoList/components/ToDoList.js
@@ -5,10 +5,10 @@ import { v4 as uuidv4 } from 'uuid';
 import AddTask from './AddTask';
 
 const ToDoList = ({ initialValues }) => {
-    const [items, setItems] = useState(initialValues.map((value) => ({ id: uuidv4(), item: value })));
+    const [items, setItems] = useState(initialValues.map((value) => ({ id: uuidv4(), title: value })));
 
     const addToDo = (newTitle) => {
-        const newItem = { id: uuidv4(), item: newTitle };
+        const newItem = { id: uuidv4(), title: newTitle };
         setItems((prevItems) => [...prevItems, newItem]);
     };
 
@@ -16,9 +16,23 @@ const ToDoList = ({ initialValues }) => {
         setItems((prevItems) => prevItems.filter((item) => item.id !== id));
     };
 
+    return (
+        <View style={styles.container}>
+          {items.map((item) => (
+            <View key={item.id}>
+              <Text style={styles.text}>ToDo item: {item.title}</Text>
+              <View style={styles.buttonContainer}>
+                <Button title="Remove" onPress={() => removeToDo(item.id)} />
+              </View>
+            </View>
+          ))}
+          {/* <AddTask onAddTask={addToDo} /> */}
+        </View>
+    );
+
 };
 
-Counter.defaultProps = {
+ToDoList.defaultProps = {
     initialValues: [],
 };
 
